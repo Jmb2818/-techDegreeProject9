3bbbafe8b15a56f47e760779c710c1aa407bdd89
Joshua Borck <borck1010@gmail.com>
2019-06-30 12:38:42 -0600

    *UI updates

--- Proximity/MapViewController.swift
+++ Proximity/MapViewController.swift
@@ -11,10 +11,57 @@ import MapKit
 
 class MapViewController: UIViewController {
     
+    // MARK: IBOutlets
     @IBOutlet weak var mapView: MKMapView!
     
+    // MARK: Properties
+    private let locationManager = CLLocationManager()
+    private let regionRadius: CLLocationDistance = 1000
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        focusOnCurrentLocation()
+    }
+    
+    /// Funcion to zoom in on users current location if permission has been given
+    func focusOnCurrentLocation() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            guard let currentLocation = locationManager.location else {
+                locationManager.requestLocation()
+                return
+            }
+            
+            let coordinateRegion = MKCoordinateRegion(center: currentLocation.coordinate,
+                                                      latitudinalMeters: regionRadius,
+                                                      longitudinalMeters: regionRadius)
+            
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+    }
+}
+
+// MARK: CLLocationManagerDelegate Conformance
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        focusOnCurrentLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            focusOnCurrentLocation()
+        default:
+            break
+        }
     }
 }
